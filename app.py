2e377c77b571f6dd082e4e4c9ce7aea360d0e29e
alifia085872728663-spec <alifia085872728663@gmail.com>
2026-05-23 10:10:49 +0700

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+# app.py — baris 13
+st.set_page_config(
+  page_title="Finance Dashboard",
+  layout="wide"
+)
 # Hirarki teks
 st.title("📊 Dashboard")
 st.header("Laporan Bulanan")
